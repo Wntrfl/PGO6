9fb2207aa65fdb494c9f13f3808fca0df2ed7b07
Wntrfl <s35079@pjwstk.edu.pl>
2026-04-14 10:47:37 +0200

    Created Employee class and tested

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public static void main(String[] args) {
 
-Employee e1 = new Employee("C1234", "Ivan", "Asddff", 12000);
+    Employee e1 = new Employee("C1234", "Ivan", "Asddff", 12000);
 
-System.out.println(e1.introduceYourself());
+    System.out.println(e1.introduceYourself());
 
+    Employee e2 = new Employee("B3455", "Maria", "XCVB", 12000);
+
+    System.out.println(e2.introduceYourself());
 }
