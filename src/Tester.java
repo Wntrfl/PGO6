3d77f603ea8143d9e8139142efb9d0df69c9e314
Wntrfl <s35079@pjwstk.edu.pl>
2026-04-20 20:33:04 +0200

    Created Tester class and tested

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,36 @@
-public class Tester {
+public class Tester extends Employee {
+    private boolean automationTester;
+    private int scenarioCount;
+
+    public Tester(String employeeId, String firstName, String lastName,
+                  double baseSalary, boolean automationTester, int scenarioCount){
+        super(employeeId, firstName, lastName, baseSalary);
+        this.automationTester = automationTester;
+        this.scenarioCount = scenarioCount;
+    }
+
+    @Override
+    public double calculateMonthlyCost(){
+        return this.getBaseSalary()*1.6;
+
+    }
+
+    @Override
+    public String introduceYourself(){
+        return "Hello, I am tester";
+    }
+
+    public void runTestingReport(){
+        System.out.println("Testing");
+    }
+
+    public boolean getAutomationTester(){
+        return this.automationTester;
+    }
+
+    public int getScenarioCount(){
+        return this.scenarioCount;
+    }
+
+
 }
